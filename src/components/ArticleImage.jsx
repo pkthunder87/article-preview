d232c308d631c97ac32a1e9d0e05371404db47c7
pkthunder87 <pkthunder87@gmail.com>
2025-01-28 07:39:11 -0800

    Feat: Add responsive design for 1400px and beyond

--- src/components/ArticleImage.jsx
+++ src/components/ArticleImage.jsx
@@ -1,10 +1,12 @@
 function ArticleImage() {
   return (
-    <img
-      className="article-image"
-      src="./assets/images/drawers.jpg"
-      alt="Cozy green drawers with a vase of wheat on top"
-    />
+    <div className="article--left">
+      <img
+        className="article-image"
+        src="./assets/images/drawers.jpg"
+        alt="Cozy green drawers with a vase of wheat on top"
+      />
+    </div>
   );
 }
 
